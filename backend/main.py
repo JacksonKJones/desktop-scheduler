56b6036c938aac324b1dfa6cb65cb65098822c34
Jackson <jacksonkjones@protonmail.com>
2025-05-30 22:36:04 -0400

    Created classes for Tasks and Schedules in models.py
    The Schedule Class has methods for
    - Adding tasks
    - Removing tasks
    - Returning tasks

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 # App Entry Point
 import tkinter as tk
+from models import Schedule, Task
+from datetime import time
 
 def main():
     root = tk.Tk()
     root.title("Scheduler Desktop")
     label = tk.Label(root, text="Schedule...")
-    label.pack(padx=200,pady=200)
+    label.pack(padx=200,pady=400)
     root.mainloop()
 
 if __name__ == "__main__":
---
+++ backend/models.py
@@ -0,0 +1,34 @@
+from datetime import time
+
+# Define the Task Class
+class Task:
+    def __init__(self, task_name: str, task_time: time, task_description: str=""):
+        self.task_name = task_name
+        self.task_time = task_time # Receives a datetime.time object
+        self.task_description = task_description
+
+    def __repr__(self):
+        return f"<Task(name={self.task_name}, time={self.task_time}, description={self.task_description})>"
+
+
+
+# Define the Schedule Class
+class Schedule:
+    def __init__(self, schedule_name: str):
+        self.schedule_name = schedule_name
+        self.schedule_tasks = [] # List of Task Objects
+    
+    def add_task(self, task: Task):
+        self.schedule_tasks.append(task)
+
+    def remove_task(self, task: str):
+        self.schedule_tasks = [i for i in self.schedule_tasks if i.task_name != task]
+
+    def get_task(self, task_name: str):
+        for task in self.schedule_tasks:
+            if task.schedule_name == task_name:
+                return task
+        return None
+    
+    def __repr__(self):
+        return f"<Schedule(name={self.schedule_name}, tasks={len(self.schedule_tasks)})>"
